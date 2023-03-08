5b6b77935799883346f46b660abcdd65fb21a97c
Freitas-MA <123067492+Freitas-MA@users.noreply.github.com>
2023-03-08 13:15:24 +0000

    Update js

--- Projetos lvl3 js/Testes JS/src/css/script.js
+++ Projetos lvl3 js/Testes JS/src/css/script.js
@@ -1,75 +1,24 @@
-/*
-var media = null 
-var nota1 = prompt('Digite a nota 1:')
-var nota2 = prompt('Digite a nota 2:')
-var notafinal = 6
-
-nota1 = parseInt(nota1)
-nota2 = parseInt(nota2)
-
-media = (nota1 + nota2) / 2;
+var num1 = prompt('Entre com o primeiro numero:')
+var oper = prompt('Selecione a operacao: (Somar/Subtrair)')
+var num2 = prompt('Entre com o segundo numero:')
+var result = null
+num1 = parseInt(num1)
+num2 = parseInt(num2)
 
 
-document.write (media)
-document.write (' <hr/>')
-if (media >= notafinal ) { 
-    document.write ('Aluno Aprovado')
-}
-else { 
-    document.write ('Aluno reprovado')
-}
-*/
 
+function calc(num1, num2, oper) {
+        switch (oper) {
 
- 
-/*
-if (idade >= 0 && idade < 15) { 
-    document.write ('Crianca')
+        case 'Somar':
+            result = num1 + num2
+            break;
+        case 'Subtrair':
+            result = num1 - num2
+            break;
+        default:
+            result = ('Operacao invalida, tente novamente.')
+        }
+        return result;
 }
-if (idade >= 15 && idade < 30) {
-    document.write ('Jovem')
-}
-if (idade >= 30 && idade < 60) {
-    document.write ('Adulto')
-}
-if (idade >= 60) {
-    document.write ('idoso')
-}
-*/
-
-var nome = prompt('Nome:')
-var peso = prompt('peso: kg')
-var altura = prompt('altura: cm')
-var imc = null
-
-peso = parseFloat(peso)
-altura = parseFloat(altura / 100)
-
-imc = peso  / (altura * altura)
-
-imc = parseFloat(imc.toFixed(2))
-
-if (imc < 16) {
-    document.write ('Ola ' + nome + ', seu IMC e de ' + imc +': Baixo peso, muito grave.')
-}
-if (imc >= 16 && imc < 16.99) {
-    document.write ('Ola ' + nome + ', seu IMC e de ' + imc +': Baixo peso, grave.')
-}
-if (imc >= 17 && imc < 18.49) {
-    document.write ('Ola ' + nome + ', seu IMC e de ' + imc +': Baixo peso.')
-}
-if (imc >= 18.50 && imc < 24.99) {
-    document.write ('Ola ' + nome + ', seu IMC e de ' + imc +': Peso normal.')
-}
-if (imc >= 25 && imc < 29.99) {
-    document.write ('Ola ' + nome + ', seu IMC e de ' + imc +': Sobrepeso.')
-}
-if (imc >= 30 && imc < 34.99) {
-    document.write ('Ola ' + nome + ', seu IMC e de ' + imc +': Obesidade grau I.')
-}
-if (imc >= 35 && imc < 39.99) {
-    document.write ('Ola ' + nome + ', seu IMC e de ' + imc +': Obesidade grau II.')
-}
-if (imc >= 40) {
-    document.write ('Ola ' + nome + ', seu IMC e de ' + imc +': Obesidade gray III.')
-}
+document.write('O resultado final e ' +calc(num1, num2, oper))
